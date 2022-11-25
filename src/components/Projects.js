918a706d26bd31b5093d91e5c3896506d664eceb
iamSamuo <samuokim@gmail.com>
2022-11-25 08:23:32 +0300

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,29 +13,8 @@ const Projects = () => {
         With the programming skills i have aquired so far.
         <br /> <br /> I have managed to work on the following projects:
       </p>
+
       <div className="project-items">
-        <div className="item">
-          <img src={cocktails} alt="cocktail" />
-          <h1 className="project-p-h1">Cocktail Joint</h1>
-          <p className="project-p-h1">
-            Cocktail joint is a project made with ReactJs. This site uses an
-            external API to fetch data. Users are able to search different types
-            of cocktails through the search bar, as well as get detailed
-            information on cocktails ingredients
-          </p>
-          <a href="https://cocktail-joint.netlify.app">View Project</a>
-        </div>
-
-        <div className="item">
-          <img src={mindgame} alt="mindgame" />
-          <h1 className="project-p-h1">Mind Game</h1>
-          <p className="project-p-h1">
-            This is a JavaScript paractice project, that i built while learning
-            JavaScript programming language
-          </p>
-          <a href="https://samuocodes-mindgame.netlify.app">View Project</a>
-        </div>
-
         <div className="item">
           <img src={linkedin} alt="linkedin" />
           <h1 className="project-p-h1">LinkedIn Clone</h1>
@@ -47,21 +26,11 @@ const Projects = () => {
             the technologies used in this build are: ReactJs, Redux, Firebase
             and Material UI for different icons used in the app.
           </p>
-          <a href="https://linkedin-clone-3527d.firebaseapp.com/">
+          <a href="https://samuocodes-linkedin-clone.netlify.app">
             View Project
           </a>
         </div>
 
-        <div className="item">
-          <img src={hotelmenu} alt="hotelmenu" />
-          <h1 className="project-p-h1">React Hotel Menu</h1>
-          <p className="project-p-h1">
-            A Hotel Menu project that allows users to search for different meals
-            to order, depending on time,that is; braekfast,lunch as well as
-            shakes
-          </p>
-          <a href="https://samuocodes-hotel-menu.netlify.app">View Project</a>
-        </div>
         <div className="item">
           <img src={netflixclone} alt="netflixclone" />
           <h1 className="project-p-h1">React Neflix Clone</h1>
@@ -72,6 +41,38 @@ const Projects = () => {
           </p>
           <a href="https://sammy-netflixclone.netlify.app/">View Project</a>
         </div>
+      </div>
+
+      <div className="item">
+        <img src={cocktails} alt="cocktail" />
+        <h1 className="project-p-h1">Cocktail Joint</h1>
+        <p className="project-p-h1">
+          Cocktail joint is a project made with ReactJs. This site uses an
+          external API to fetch data. Users are able to search different types
+          of cocktails through the search bar, as well as get detailed
+          information on cocktails ingredients
+        </p>
+        <a href="https://cocktail-joint.netlify.app">View Project</a>
+      </div>
+
+      <div className="item">
+        <img src={mindgame} alt="mindgame" />
+        <h1 className="project-p-h1">Mind Game</h1>
+        <p className="project-p-h1">
+          This is a JavaScript paractice project, that i built while learning
+          JavaScript programming language
+        </p>
+        <a href="https://samuocodes-mindgame.netlify.app">View Project</a>
+      </div>
+
+      <div className="item">
+        <img src={hotelmenu} alt="hotelmenu" />
+        <h1 className="project-p-h1">React Hotel Menu</h1>
+        <p className="project-p-h1">
+          A Hotel Menu project that allows users to search for different meals
+          to order, depending on time,that is; braekfast,lunch as well as shakes
+        </p>
+        <a href="https://samuocodes-hotel-menu.netlify.app">View Project</a>
       </div>
     </section>
   );
